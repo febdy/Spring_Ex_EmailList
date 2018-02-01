9956781a4b67d6495a001f9553d64c18732af2f0
febdy <feb29dy@gmail.com>
2018-02-01 14:45:04 +0900

    add mybatis.

--- src/main/java/com/javaex/dao/EmaillistDao.java
+++ src/main/java/com/javaex/dao/EmaillistDao.java
@@ -1,112 +1,26 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.EmailVo;
 
-import oracle.jdbc.pool.OracleDataSource;
-
 @Repository
 public class EmaillistDao {
 
 	@Autowired
-	private OracleDataSource oracleDataSource = null;
-	
-	private Connection conn = null;
-	private PreparedStatement pstmt = null;
-	private ResultSet rs = null;
-		
-	public void insert(EmailVo vo) {
-		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+	private SqlSession sqlSession;
 
-			// 2. Connection 얻어오기
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-
-			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "INSERT INTO emaillist "
-						 + "VALUES (seq_emaillist_no.nextval, ?, ?, ?)";
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, vo.getLastName());
-			pstmt.setString(2, vo.getFirstName());
-			pstmt.setString(3, vo.getEmail());
-			int cnt = pstmt.executeUpdate();
-
-			System.out.println(cnt + "건 저장완료");
-			
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-		}
+	public void insert(EmailVo emailVo) {		
+		sqlSession.insert("emaillist.insert", emailVo);
 	}
-	
+
 	public List<EmailVo> getList() {
-		List<EmailVo> eList = new ArrayList<>();
-		EmailVo vo;
-		
-		try {
-			conn = oracleDataSource.getConnection();
+		List<EmailVo> eList = sqlSession.selectList("emaillist.getList");
 
-			// 3. SQL문 준비 / 바인딩 / 실행
-			String query = "SELECT no, last_name, first_name, email " +
-						   " FROM emaillist" +
-						   " ORDER BY no";
-			pstmt = conn.prepareStatement(query);
-			pstmt.executeQuery();
-			rs = pstmt.getResultSet();
-
-			while (rs.next()) {
-				vo = new EmailVo();
-				vo.setNo(rs.getInt("no"));
-				vo.setLastName(rs.getString("last_name"));
-				vo.setFirstName(rs.getString("first_name"));
-				vo.setEmail(rs.getString("email"));
-				eList.add(vo);				
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			// 5. 자원정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-		}
-		
 		return eList;
 	}
 
